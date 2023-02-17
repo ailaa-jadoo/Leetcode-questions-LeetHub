6614583eed2facebaee13c9ad61fdfbd79c3de70
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-02-17 11:59:41 +0530

    Time: 0 ms (100.00%), Space: 9.7 MB (69.86%) - LeetHub

--- 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
+++ 0783-minimum-distance-between-bst-nodes/0783-minimum-distance-between-bst-nodes.cpp
@@ -11,27 +11,24 @@
  */
 class Solution {
 public:
-    void inorder(TreeNode* root, vector<int> &res){
-        if(root){
-            inorder(root->left, res);
-            res.push_back(root->val);
-            inorder(root->right, res);
-        }
+    int minDistance = INT_MAX;
+    TreeNode* prevValue;
+        
+    void inorderTraversal(TreeNode* root) {
+        if (root == NULL)
+            return;
+        
+        inorderTraversal(root->left);
+        
+        if (prevValue != NULL) 
+            minDistance = min(minDistance, root->val - prevValue->val);
+        
+        prevValue = root;        
+        inorderTraversal(root->right);
     }
     
     int minDiffInBST(TreeNode* root) {
-        
-        vector<int> res;
-        inorder(root, res);
-        
-        int diff = INT_MAX;
-        
-        sort(res.begin(), res.end());
-        for (int i = 0; i < res.size() - 1; i++)
-            if (res[i + 1] - res[i] < diff)
-                diff = res[i + 1] - res[i];
-        
-        
-        return diff;
-    }    
+        inorderTraversal(root);
+        return minDistance;
+    }
 };
