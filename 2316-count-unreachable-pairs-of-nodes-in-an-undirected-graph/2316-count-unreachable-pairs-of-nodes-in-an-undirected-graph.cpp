fb8cf8ee0c2b69a595611d1fbc98c8ac0493ca0e
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-03-25 17:42:51 +0530

    Time: 624 ms (64.94%), Space: 281.9 MB (10.52%) - LeetHub

---
+++ 2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph/2316-count-unreachable-pairs-of-nodes-in-an-undirected-graph.cpp
@@ -0,0 +1,49 @@
+class Solution {
+public:
+    
+    int bfs(int node, vector<vector<int>>& adj, vector<bool>& visit){
+        queue<int> q;
+        q.push(node);
+        int count = 1;
+        visit[node] = true;
+        
+        while(!q.empty()){
+            node = q.front();
+            q.pop();
+            for(int neigbour :adj[node]){
+                if(!visit[neigbour]){
+                    visit[neigbour] = true;
+                    count++;
+                    q.push(neigbour);
+                }
+            }
+        }
+        
+        
+        return count;
+    }
+    
+    long long countPairs(int n, vector<vector<int>>& edges) {
+        vector<vector<int>> adj(n);
+        for(auto& edge:edges){
+            adj[edge[0]].push_back(edge[1]);
+            adj[edge[1]].push_back(edge[0]);
+        }
+        
+        long long NoOfPairs = 0;
+        long long sizeOfComponent = 0;
+        long long remainingNodes = n;
+        
+        vector<bool> visit(n);
+        
+        for(int i=0;i<n;i++){
+            if(!visit[i]){
+                sizeOfComponent = bfs(i, adj, visit);
+                NoOfPairs += sizeOfComponent*(remainingNodes - sizeOfComponent);
+                remainingNodes -= sizeOfComponent;
+            }
+        }
+        
+        return NoOfPairs;
+    }
+};
