18c232fa3f98cea26c4bd177b47ecc2ce9793f7e
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-05-28 18:06:57 +0530

    Time: 130 ms (15.43%), Space: 10.2 MB (60.72%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.cpp
@@ -1,36 +1,23 @@
 class Solution {
-private:
-  int helper(int i, int j, vector<int> &cuts,  vector<vector<int>> &dp){
-    
-    
-    if(i>j)
-        return 0;
-        
-    if(dp[i][j]!=-1)
-        return dp[i][j];
-    
-    int minm = INT_MAX;
-    
-    for(int ind=i; ind<=j; ind++){
-        
-        int cost = cuts[j+1] - cuts[i-1] +  helper(i,ind-1,cuts,dp) + helper(ind+1,j,cuts,dp);
-        minm = min(minm, cost);
-        
-    }
-    
-    return dp[i][j] = minm;
-  }
-  
 public:
     int minCost(int n, vector<int>& cuts) {
-         
-         int c = cuts.size();
-         cuts.insert(cuts.begin(),0);
-         cuts.push_back(n);
+        cuts.push_back(0);
+        cuts.push_back(n);
+        sort(cuts.begin(), cuts.end());
 
-         sort(cuts.begin(),cuts.end());
-         vector<vector<int>> dp(c+1,vector<int>(c+1,-1));
+        int m = cuts.size();
+        vector<vector<int>> dp(m, vector<int>(m, 0));
 
-        return helper(1,c,cuts,dp);
+        for (int len = 2; len <= m; len++) {
+            for (int i = 0; i + len < m; i++) {
+                int j = i + len;
+                dp[i][j] = INT_MAX;
+                for (int k = i + 1; k < j; k++) {
+                    dp[i][j] = min(dp[i][j], dp[i][k] + dp[k][j] + cuts[j] - cuts[i]);
+                }
+            }
+        }
+
+        return dp[0][m - 1];
     }
 };
