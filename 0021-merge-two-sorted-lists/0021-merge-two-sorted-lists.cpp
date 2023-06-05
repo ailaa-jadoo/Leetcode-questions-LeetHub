d8fa338c118de439243b3010fc86db0c174fcf39
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-06-05 22:56:40 +0530

    Time: 8 ms (70.17%), Space: 14.8 MB (67.88%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -10,28 +10,61 @@
  */
 class Solution {
 public:
+//     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+//         if(!list1)  
+//             return list2;
+//         if(!list2)  
+//             return list1;
+        
+//         if(list1->val > list2->val)
+//             swap(list1, list2);
+        
+//         ListNode* res = list1;
+        
+//         while(list1!=NULL and list2!=NULL){
+//             ListNode* temp = NULL;
+//             while(list1!=NULL and list1->val<=list2->val){
+//                 temp = list1;
+//                 list1 = list1->next;
+//             }
+            
+//             temp->next = list2;
+//             swap(list1, list2);
+//         }
+        
+//         return res;
+//     }
+    
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        
         if(!list1)  
             return list2;
         if(!list2)  
             return list1;
         
-        if(list1->val > list2->val)
-            swap(list1, list2);
-        
-        ListNode* res = list1;
+        ListNode *temp, *dummy;
+        temp = new ListNode(101);
+        dummy = temp;
         
         while(list1!=NULL and list2!=NULL){
-            ListNode* temp = NULL;
-            while(list1!=NULL and list1->val<=list2->val){
-                temp = list1;
+            if(list1->val <= list2->val){
+                dummy->next = list1;
                 list1 = list1->next;
             }
+            else{
+                dummy->next = list2;
+                list2 = list2->next;
+            }
             
-            temp->next = list2;
-            swap(list1, list2);
+            dummy = dummy->next;
         }
         
-        return res;
+        
+        if(!list1)  
+            dummy->next = list2;
+        if(!list2)  
+            dummy->next = list1;
+        
+        return temp->next;
     }
 };
