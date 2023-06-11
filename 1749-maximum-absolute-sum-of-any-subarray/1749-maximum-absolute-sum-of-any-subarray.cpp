fa7ab8f68110452adb373bffe7fbb27b3b704039
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-06-11 23:24:19 +0530

    Time: 81 ms (17.46%), Space: 41.5 MB (22.44%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    int maxAbsoluteSum(vector<int>& nums) {
-        int n = nums.size();
-        vector<int> maxi;
-        vector<int> mini;
+int maxAbsoluteSum(std::vector<int>& nums) {
+    int n = nums.size();
+    
+    int maxSum = nums[0];
+    int minSum = nums[0];
+    int currMax = nums[0];
+    int currMin = nums[0];
+    int maxAbsSum = std::abs(nums[0]);
+    
+    for (int i = 1; i < n; i++) {
+        currMax = std::max(nums[i], currMax + nums[i]);
+        maxSum = std::max(maxSum, currMax);
         
-        maxi.push_back(nums[0]); mini.push_back(nums[0]);
+        currMin = std::min(nums[i], currMin + nums[i]);
+        minSum = std::min(minSum, currMin);
         
-        for(int i=1;i<n;i++){
-            int max_sum = maxi[i-1]+nums[i];
-            maxi.push_back(max(max_sum, nums[i]));
-            int min_sum = mini[i-1]+nums[i];
-            mini.push_back(min(min_sum, nums[i]));
-        }
-        
-        int max_num_1 = INT_MIN, max_num_2 = INT_MAX;
-        
-        for(int num:maxi)
-            max_num_1 = max(num, max_num_1);
-        
-        for(int num:mini)
-            max_num_2 = min(num, max_num_2);
-        
-        cout<<max_num_1<<" "<<max_num_2<<" ";
-        
-        return max(max_num_1, abs(max_num_2)); 
+        maxAbsSum = std::max(maxAbsSum, std::max(std::abs(maxSum), std::abs(minSum)));
     }
+    
+    return maxAbsSum;
+}
 };
+
+
+// https://youtu.be/XfCOrINuo4c    idea issne diya
