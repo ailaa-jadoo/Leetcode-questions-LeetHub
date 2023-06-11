0e1561dfae79f0f819b02c8897d9136a019f6820
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-06-11 22:04:02 +0530

    Time: 144 ms (5.04%), Space: 6 MB (70.93%) - LeetHub

---
+++ 1742-maximum-number-of-balls-in-a-box/1742-maximum-number-of-balls-in-a-box.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int digiSum(int n){
+        string ch = to_string(n);
+        int sum = 0;
+        for(int i=0;i<ch.size();i++){
+            sum = sum + (ch[i]-'0');
+        }
+        
+        return sum;
+    }
+    
+    int countBalls(int lowLimit, int highLimit) {
+        vector<int> boxes(60, 0);
+        int maxi=INT_MIN;
+        
+        for(int i=lowLimit;i<=highLimit;i++)
+            boxes[digiSum(i)]++;
+        
+        for(int box:boxes)
+            maxi = max(maxi, box);
+        
+        return maxi;
+    }
+};
