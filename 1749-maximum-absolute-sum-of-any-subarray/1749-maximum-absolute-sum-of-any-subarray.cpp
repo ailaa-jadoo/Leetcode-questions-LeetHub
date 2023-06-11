49bc23d70dda624751dd2529e498491b4961a654
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-06-11 23:25:59 +0530

    Time: 77 ms (33.42%), Space: 41.4 MB (22.44%) - LeetHub

--- 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
+++ 1749-maximum-absolute-sum-of-any-subarray/1749-maximum-absolute-sum-of-any-subarray.cpp
@@ -1,27 +1,25 @@
 class Solution {
 public:
-int maxAbsoluteSum(std::vector<int>& nums) {
-    int n = nums.size();
-    
-    int maxSum = nums[0];
-    int minSum = nums[0];
-    int currMax = nums[0];
-    int currMin = nums[0];
-    int maxAbsSum = std::abs(nums[0]);
-    
-    for (int i = 1; i < n; i++) {
-        currMax = std::max(nums[i], currMax + nums[i]);
-        maxSum = std::max(maxSum, currMax);
-        
-        currMin = std::min(nums[i], currMin + nums[i]);
-        minSum = std::min(minSum, currMin);
-        
-        maxAbsSum = std::max(maxAbsSum, std::max(std::abs(maxSum), std::abs(minSum)));
+//     gpt ne chota karke diya
+    int maxAbsoluteSum(vector<int>& nums) {
+        int n = nums.size();
+
+        int maxSum = nums[0];
+        int minSum = nums[0];
+        int currMax = nums[0];
+        int currMin = nums[0];
+        int maxAbsSum = abs(nums[0]);
+
+        for (int i = 1; i < n; i++) {
+            currMax = max(nums[i], currMax + nums[i]);
+            maxSum = max(maxSum, currMax);
+
+            currMin = min(nums[i], currMin + nums[i]);
+            minSum = min(minSum, currMin);
+
+            maxAbsSum = max(maxAbsSum, max(abs(maxSum), abs(minSum)));
+        }
+
+        return maxAbsSum;
     }
-    
-    return maxAbsSum;
-}
 };
-
-
-// https://youtu.be/XfCOrINuo4c    idea issne diya
