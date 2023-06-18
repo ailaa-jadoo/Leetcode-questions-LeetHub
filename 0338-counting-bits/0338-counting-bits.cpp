2b84fb9af84c895a3f6a91fc6c67121e61e27cfc
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-06-18 22:36:26 +0530

    Time: 4 ms (83.82%), Space: 7.7 MB (99.67%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,18 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> res;
-        for(int i=0;i<=n;i++){
-            int count1s =0;
-            bitset<32> binaryNumber(i);
-            string s = binaryNumber.to_string();
-            for(int j=0;j<s.size();j++){
-                if(s[j]=='1')
-                    count1s++;
-            }
-            
-            res.push_back(count1s);
-        }
+        vector<int> res(n+1, 0);
+        
+        for(int i=1;i<n+1;i++)
+            res[i] = res[i>>1]+(i&1);
         
         return res;
     }
