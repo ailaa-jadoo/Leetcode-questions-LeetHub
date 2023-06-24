6f6f8a89722bfa947cb067e27168998632e11d9e
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-06-24 22:27:48 +0530

    Time: 268 ms (34.36%), Space: 64.2 MB (50.66%) - LeetHub

--- 0956-tallest-billboard/0956-tallest-billboard.cpp
+++ 0956-tallest-billboard/0956-tallest-billboard.cpp
@@ -1,35 +1,20 @@
-const int MIN = -1e4, offset = 5000;
 class Solution {
-    int ans, n;
-    vector<int> rods;
-    int dp[21][2 * offset + 1];
 public:
     int tallestBillboard(vector<int>& rods) {
-        this->rods = rods;
-        n = rods.size();
-        memset(dp , -1 , sizeof(dp));
-        int ans = solve(0, 0);
-        if (ans < 0)
-            return 0;
-        else 
-            return ans;
-    }
-    
-    int solve(int i, int diff) {
-        if (i == n) {
-            if (diff == 0)
-                return 0;
-            return INT_MIN;
+        unordered_map<int, int> t;
+        t[0] = 0;
+
+        for (int x : rods) {
+            unordered_map<int, int> cur_t = t;
+            for (auto it = cur_t.begin(); it != cur_t.end(); ++it) {
+                int d = it->first;
+                int y = it->second;
+
+                t[d + x] = max(t[d + x], y);
+                t[abs(d - x)] = max(t[abs(d - x)], y + min(d, x));
+            }
         }
-        
-        int &ans = dp[i][diff + offset];
-        if (ans != -1)
-            return ans;
-        
-        int opt1 = solve(i + 1 , diff);
-        int opt2 = rods[i] + solve(i + 1 , diff + rods[i]);
-        int opt3 = solve(i + 1 , diff - rods[i]);
-        
-        return ans = max({opt1, opt2, opt3});
+
+        return t[0];
     }
 };
