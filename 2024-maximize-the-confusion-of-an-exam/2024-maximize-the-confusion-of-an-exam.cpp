fa413bf1a2558b1fc26f0a68de63a798ab71f15a
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-07 21:10:46 +0530

    Time: 35 ms (37.07%), Space: 12 MB (14.43%) - LeetHub

--- 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -1,36 +1,24 @@
 class Solution {
 public:
-    int maxConsecutiveAnswers(string s, int k) {
-        
+    
+    int maxLen(string s, int k, char ch){
         int left =0, cnt=0, res=0;
-        
         for(int right=0;right<s.size();right++){
-            if(s[right]=='F')
+            if(s[right]==ch)
                 cnt++;
             
             while(cnt>k){
-                if(s[left]=='F')
+                if(s[left]==ch)
                     cnt--;
                 left++;
             }
             
             res = max(res, right-left+1);
         }
-        
-        cnt=0, left=0;
-        for(int right=0;right<s.size();right++){
-            if(s[right]=='T')
-                cnt++;
-            
-            while(cnt>k){
-                if(s[left]=='T')
-                    cnt--;
-                left++;
-            }
-            
-            res = max(res, right-left+1);
-        }
-        
         return res;
+    }
+    
+    int maxConsecutiveAnswers(string s, int k) {      
+        return max(maxLen(s,k,'T'), maxLen(s,k,'F'));
     }
 };
