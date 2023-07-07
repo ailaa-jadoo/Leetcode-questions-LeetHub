315630fa96b962bfc8817df060f35a0a33f7f01c
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-07 21:22:36 +0530

    Time: 15 ms (52.47%), Space: 9.1 MB (5.24%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    int maxLen(string s, int k, char ch){
+        int left =0, cnt=0, res=0;
+        
+        for(int right = 0;right<s.size();right++){
+            if(s[right] != ch)
+                cnt++;
+            while(cnt>k){
+                if(s[left]!=ch)
+                    cnt--;
+                left++;
+            }
+            
+            res = max(res, right-left+1);
+        }
+        
+        return res;
+    }
+    
+    int characterReplacement(string s, int k) {
+        int res = 0;
+        vector<int> count(27,0);
+        for(int i=0;i<s.size();i++)
+            count[s[i]-'A']++;
+                  
+        for(char ch = 'A'; ch<='Z';ch++)
+            if(count[ch-'A']!=0){
+                cout<<ch<<" "<<count[ch-'A']<<endl;
+                res = max(res, maxLen(s,k,ch));
+            }
+        
+        return res;
+    }
+};
