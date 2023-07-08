beecbf6a657b5702a727f7310cc3cd1575d9c863
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-08 21:32:53 +0530

    Time: 27 ms (52.57%), Space: 12.3 MB (38.34%) - LeetHub

---
+++ 0494-target-sum/0494-target-sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        int sum = accumulate(nums.begin(), nums.end(), 0);
+        int n = nums.size();
+        
+        if((target+sum)%2!=0 or (sum-target)<0)
+            return 0;
+        
+        if(abs(target)>sum)
+            return 0;
+        
+        //target = target<0 ? -1*target: target;
+        
+        int s1 = (target+sum)/2;
+        
+        vector<vector<int>> t(n+1, vector<int>(s1+1, 0));
+        
+        t[0][0] = 1;
+        
+        // if(nums[0]==0)
+        //     t[0][0]=2;
+        // else
+        //     t[0][0]=1;
+        // if(nums[0]!=0 && nums[0]<=s1)
+        //     t[0][nums[0]]=1;        
+        
+        for(int i=1;i<=n;i++) 
+            for(int j=0;j<=s1;j++){
+                if(nums[i-1]<=j)
+                    t[i][j] = t[i-1][j] + t[i-1][j - nums[i-1]];
+                else
+                    t[i][j] = t[i-1][j];
+            }
+        
+        return t[n][s1];
+    }
+};
