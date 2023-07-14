bdb0d0c9a6e9b47b71e1129abcb37c6a0d79e792
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-14 22:31:22 +0530

    Time: 431 ms (21.48%), Space: 85.5 MB (63.27%) - LeetHub

--- 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
+++ 0718-maximum-length-of-repeated-subarray/0718-maximum-length-of-repeated-subarray.cpp
@@ -6,21 +6,23 @@ public:
         
         vector<vector<int>> t(n+1, vector<int>(m+1, 0));
         
+        int maxi = 0;
+        
         for(int i=0;i<n+1;i++)
             t[i][0] = 0;
         
         for(int i=1;i<n+1;i++)
             for(int j=1;j<m+1;j++){
-                if(nums1[i-1]==nums2[j-1])
+                if(nums1[i-1]==nums2[j-1]){
                     t[i][j] = 1 + t[i-1][j-1];
+                    
+                }                    
                 else
                     t[i][j] = 0;
+                
+                maxi = max(maxi, t[i][j]);
             }
         
-        int maxi = INT_MIN;
-        for(int i=0;i<n+1;i++)
-            for(int j=0;j<m+1;j++)
-                maxi = max(maxi, t[i][j]);
         
         return maxi;                            
     }
