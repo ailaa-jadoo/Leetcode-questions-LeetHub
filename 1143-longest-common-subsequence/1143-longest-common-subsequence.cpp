05adc4264a60be35b2684b2920b4d6a698c9c5bc
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-14 21:19:11 +0530

    Time: 62 ms (68.99%), Space: 24.5 MB (46.72%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,20 +1,23 @@
 class Solution {
-public:   
-    int LCS(string &s1, string &s2, int n, int m, vector<vector<int>>& t){ 
-        if(n == 0 || m == 0) 
-            return 0;
-        
-        if(t[n][m] != -1) 
-            return t[n][m];
-        
-        if(s1[n-1] == s2[m-1])
-            return t[n][m] = 1 + LCS( s1, s2, n-1, m-1, t);
-        else
-            return t[n][m] = max(LCS( s1, s2, n, m-1, t), LCS( s1, s2, n-1, m, t));
-    }
+public:
     int longestCommonSubsequence(string text1, string text2) {
-        int n = text1.size() , m = text2.size();
-        vector<vector<int>> t(n+1, vector<int>(m+1, -1));
-        return LCS(text1, text2, n, m, t);
+        int n = text1.size();
+        int m = text2.size();
+        vector<vector<int>> t(n+1, vector<int>(m+1, 0));
+        
+        for(int i=0;i<n+1;i++)
+            t[i][0]=0;
+        
+        for(int i=1;i<n+1;i++){
+            for(int j=1;j<m+1;j++){
+                if(text1[i-1] == text2[j-1])
+                    t[i][j] = 1+t[i-1][j-1];
+                else
+                    t[i][j] = max(t[i-1][j], t[i][j-1]);
+            }
+        }
+        
+        
+        return t[n][m];
     }
 };
