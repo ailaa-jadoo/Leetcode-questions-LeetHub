ac6ebb3d108cf3a0939cfd240e37f164cfc9e173
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-20 23:40:04 +0530

    Time: 14 ms (21.89%), Space: 8.2 MB (99.37%) - LeetHub

---
+++ 0496-next-greater-element-i/0496-next-greater-element-i.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
+        vector<int> res(nums1.size());
+        for(int i=0;i<nums1.size();i++){
+            auto it = find(nums2.begin(), nums2.end(), nums1[i]);
+            int idx = distance(nums2.begin(), it);
+            for(int j=idx;j<nums2.size();j++){
+                if(nums2[j]>nums1[i]){
+                    res[i] = nums2[j];
+                    break;
+                }
+                else
+                    res[i] = -1;
+            }
+            cout<<endl;
+        }
+        
+        return res;
+    }
+};
