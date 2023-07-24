91d71d11b7b371c27403752fa7d9d7ed71828f6b
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-24 18:19:35 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (62.65%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        double res = 1.0;
+        long long nn=n;
+        if(nn<0)    
+            nn*=-1;
+        
+        while(nn){
+            if(nn%2==0){
+                x = x*x;
+                nn =nn/2;
+            }
+            else{
+                res = res*x;
+                nn = nn -1;
+            }
+        }
+        
+        if(n<0) 
+            res = (double)(1.0)/ (double)(res);
+        
+        return res;
+    }
+};
