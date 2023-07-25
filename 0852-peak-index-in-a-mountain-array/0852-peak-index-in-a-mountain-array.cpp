0fabf8ffc013c919207812219df9175ad7b78551
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-25 20:34:31 +0530

    Time: 127 ms (56.09%), Space: 59.6 MB (88.25%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -6,12 +6,15 @@ public:
         while(left<right){
             int mid = left + (right-left)/2;
             
-            if(arr[mid]<arr[mid+1])
-                left = mid+1;
-            else
+            if(arr[mid]>arr[mid+1] and arr[mid]>arr[mid-1])
+                return mid;
+            
+            else if(arr[mid]>arr[mid+1] and arr[mid]<arr[mid-1])
                 right = mid;
+            else if(arr[mid]<arr[mid+1] and arr[mid]>arr[mid-1])
+                left = mid+1;
         }
         
-        return left;
+        return -1;
     }
 };
