e568f1725d43d3bb6c51f9d9f005e8c660ac8646
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-07-31 21:43:27 +0530

    Time: 15 ms (61.86%), Space: 9.1 MB (38.90%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,40 +1,23 @@
-class Solution
-{
+class Solution {
 public:
-    vector<vector<int>> memo;
-    int minDistance(string word1, string word2)
-    {
-        memo.resize(word1.length() + 1, vector<int>(word2.length() + 1, -1));
-        return minDistanceRecur(word1, word2, word1.size(), word2.size());
-    }
+    int minDistance(string s1, string s2) {
+        int n = s1.size(), m = s2.size();
 
-    int minDistanceRecur(string &word1, string &word2, int word1Index, int word2Index)
-    {
-        if (word1Index == 0)
-        {
-            return word2Index;
-        }
-        if (word2Index == 0)
-        {
-            return word1Index;
-        }
-        if (memo[word1Index][word2Index] != -1)
-        {
-            return memo[word1Index][word2Index];
-        }
-        int minEditDistance = 0;
-        if (word1[word1Index - 1] == word2[word2Index - 1])
-        {
-            minEditDistance = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-        }
-        else
-        {
-            int insertOperation = minDistanceRecur(word1, word2, word1Index, word2Index - 1);
-            int deleteOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index);
-            int replaceOperation = minDistanceRecur(word1, word2, word1Index - 1, word2Index - 1);
-            minEditDistance = min(insertOperation, min(deleteOperation, replaceOperation)) + 1;
-        }
-        memo[word1Index][word2Index] = minEditDistance;
-        return minEditDistance;
+        vector<vector<int>> t(n+1, vector<int>(m+1, 0));
+        
+        for(int i = 0 ; i<n+1 ; i++)
+            t[i][0] = i;
+        for(int j = 0 ; j<m+1 ; j++)
+            t[0][j] = j;
+        
+        for(int i=1;i<n+1;i++)
+            for(int j=1;j<m+1;j++){
+                if(s1[i-1] == s2[j-1])
+                    t[i][j] = t[i-1][j-1];
+                else
+                    t[i][j] = 1+min({t[i-1][j], t[i][j-1], t[i-1][j-1]});
+            }
+        
+        return t[n][m];
     }
 };
