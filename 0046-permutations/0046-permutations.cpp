e940b295354095cefb75733d1d034ad8f120084e
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-08-02 18:56:13 +0530

    Time: 0 ms (100.00%), Space: 7.4 MB (97.00%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,29 +1,31 @@
 class Solution {
 public:
     
-    void recurFunc(vector<int> &ds, vector<int> &nums, vector<int> &freq, vector<vector<int>> &res){
-        if(ds.size() == nums.size()){
-            res.push_back(ds);
-            return;
-        }
-        
-        for(int i=0;i<nums.size();i++){
-            if(freq[i]!=1){
-                ds.push_back(nums[i]);
-                freq[i] = 1;
-                recurFunc(ds, nums, freq, res);
-                freq[i] = 0;
-                ds.pop_back();
+    void backtrack(vector<vector<int>> &res, vector<int> &temp, vector<int> &nums, vector<bool> &used){
+        if(temp.size() == nums.size())
+            res.push_back(temp);
+        else{
+            for(int i = 0;i<nums.size();i++){
+                if(used[i]==true)
+                    continue;
+            
+                used[i] = true;
+                temp.push_back(nums[i]);
+                backtrack(res, temp, nums, used);
+                used[i] = false;
+                temp.pop_back();
             }
         }
+        
             
     }
     
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
-        vector<int> ds;
-        vector<int> freq(nums.size(), 0);
-        recurFunc(ds, nums, freq, res);
+        vector<int> temp;
+        vector<bool> used(nums.size(), false);
+        
+        backtrack(res, temp, nums, used);
         
         return res; 
     }
