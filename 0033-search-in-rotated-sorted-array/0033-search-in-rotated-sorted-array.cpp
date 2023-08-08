76143e5940b9da895bac35a34b3b81c298abd17b
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-08-08 20:35:26 +0530

    Time: 3 ms (76.59%), Space: 11.1 MB (45.03%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,26 +1,36 @@
-class Solution {
-public:
-    int search(vector<int>& nums, int target) {
-        auto it = find(nums.begin(), nums.end(), target);
-        if(it == nums.end())
-            return -1;
-        return distance(nums.begin(), it);
-    }
-};
-
 // class Solution {
 // public:
 //     int search(vector<int>& nums, int target) {
-//         int low =0, high=nums.size();
-        
-//         while(low<=high){
-//             int mid = high - (high-low)/2;
-            
-//             if(nums[mid] == target)
-//                 return mid;
-//             else if(nums[mid]){
-                
-//             }
-//         }
+//         auto it = find(nums.begin(), nums.end(), target);
+//         if(it == nums.end())
+//             return -1;
+//         return distance(nums.begin(), it);
 //     }
 // };
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int low =0, high=nums.size()-1;
+        
+        while(low<=high){
+            int mid = high - (high-low)/2;
+            
+            if(nums[mid] == target)
+                return mid;
+            else if(nums[low]<=nums[mid]){ //left sorted
+                if(nums[low]<=target and nums[mid]>=target)
+                    high = mid-1;
+                else
+                    low = mid+1;
+            }else{ //right sorted
+                if(nums[high]>=target and nums[mid]<=target)
+                    low = mid+1;
+                else
+                    high = mid-1;
+            }
+        }
+        
+        return -1;
+    }
+};
