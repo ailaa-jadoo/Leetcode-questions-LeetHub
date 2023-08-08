2f3808ad434b963bd7145250b5fae5c34682a6db
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-08-08 20:11:51 +0530

    Time: 0 ms (100.00%), Space: 11.2 MB (13.70%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int search(vector<int>& nums, int target) {
-        int low =0, high = nums.size()-1;
-        
-        while(low<=high){
-            int mid = low + (high-low)/2;
-            
-            if(nums[mid] == target)
-                return mid;
-            else if(nums[mid]>=nums[low]){
-                if(nums[mid]>=target and target>=nums[low])
-                    high=mid-1;
-                else
-                    low=mid+1;
-            }
-            else{
-                if(nums[mid]<=target and target<=nums[high])
-                    low=mid+1;
-                else
-                    high=mid-1;
-            }   
-        }
-        
-        return -1;
+        auto it = find(nums.begin(), nums.end(), target);
+        if(it == nums.end())
+            return -1;
+        return distance(nums.begin(), it);
     }
 };
+
+// class Solution {
+// public:
+//     int search(vector<int>& nums, int target) {
+//         int low =0, high=nums.size();
+        
+//         while(low<=high){
+//             int mid = high - (high-low)/2;
+            
+//             if(nums[mid] == target)
+//                 return mid;
+//             else if(nums[mid]){
+                
+//             }
+//         }
+//     }
+// };
