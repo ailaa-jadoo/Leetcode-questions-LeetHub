fbd7f7cce26631f193e0c3b1e03e9e96ed6e8014
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-08-12 21:29:29 +0530

    Time: 2 ms (68.17%), Space: 7.9 MB (25.52%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,48 +1,27 @@
-// class Solution {
-// public:
-    
-//     int paths(int m, int n, int i, int j, vector<vector<int>>& obstacleGrid, vector<vector<int>> &dp){
-//         if(i==m-1 and j==n-1)
-//             return 1;
-//         if(i>=m or j>=n)
-//             return 0;
-//         if(dp[i][j]!=-1)
-//             return dp[i][j];
-//         if(obstacleGrid[i][j]==1)
-//             return 0;
-//         else{
-//             return dp[i][j] = paths(m,n,i+1,j,obstacleGrid,dp)+paths(m,n,i,j+1,obstacleGrid,dp);
-//         }
-//     }
-    
-//     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-//         int m = obstacleGrid.size();
-//         int n = obstacleGrid[0].size();
-//         vector<vector<int>> dp(m, vector<int>(n,-1));
-        
-//         if(obstacleGrid[m-1][n-1])
-//             return 0;
-        
-//         return paths(m,n,0,0,obstacleGrid,dp);
-//     }
-// };
-
 class Solution {
-public:    
-    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
-        int m = obstacleGrid.size();
-        int n = obstacleGrid[0].size();
+public:
+    int memo(vector<vector<int>>& grid, vector<vector<int>>& t, int n, int m, int i, int j){
+        if(i==n-1 and j==m-1)
+            return 1;
+        if(i>=n or j>=m)
+            return 0;
+        if(t[i][j] != -1)
+            return t[i][j];
+        if(grid[i][j]==1)
+            return 0;
+        else
+            return t[i][j] = memo(grid, t, n, m, i+1, j) + memo(grid, t, n, m, i, j+1);
+    }    
+
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
         
-        vector<int> t(n,0);
-        t[0]=1;
+        vector<vector<int>> t(n+1, vector<int>(m+1, -1));
         
-        for(int i=0;i<m;i++)
-            for(int j=0;j<n;j++){
-                if(obstacleGrid[i][j]==1)
-                    t[j] = 0;
-                else if(j>0)
-                    t[j] += t[j-1];
-            }
-        return t[n-1];
+        if(grid[n-1][m-1] == 1)
+            return 0;
+        
+        return memo(grid, t, n, m, 0, 0);
     }
 };
