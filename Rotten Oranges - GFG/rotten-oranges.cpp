6e603783cecc35952bfe5d0d723d54a585469910
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-08-13 20:55:02 +0530

    Added solution - LeetHub

---
+++ Rotten Oranges - GFG/rotten-oranges.cpp
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution 
+{
+    public:
+    //Function to find minimum time required to rot all oranges. 
+    int orangesRotting(vector<vector<int>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+        
+        // {{r, c}, t}
+        queue<pair<pair<int,int>,int>> q;
+        int fresh = 0 , afterfresh = 0; 
+        
+        vector<vector<int>> vis(n, vector<int>(m, 0));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(grid[i][j]==2){
+                    vis[i][j] = 2;
+                    q.push({{i,j}, 0});
+                }
+                if(grid[i][j]==1)
+                    fresh++;
+            }
+        }
+        
+        int time=0;
+        int drow[] = {-1, 0, 1, 0};
+        int dcol[] = {0, 1, 0, -1};
+        
+        while(!q.empty()){
+            int r = q.front().first.first;
+            int c = q.front().first.second;
+            int t = q.front().second;
+            time = max(time , t);
+            q.pop();
+            
+            for(int i=0;i<4;i++){
+                int nr = r+drow[i];
+                int nc = c+dcol[i];
+                
+                if(nr>=0 and nr<n and nc>=0 and nc<m and grid[nr][nc]==1 and vis[nr][nc]==0){
+                    vis[nr][nc] = 2;
+                    q.push({{nr, nc}, t+1});
+                    afterfresh++;
+                }
+            }
+        }
+        
+        if(fresh!=afterfresh)
+            return -1;
+        
+        return time;
+    }
+};
+
+//{ Driver Code Starts.
+int main(){
+	int tc;
+	cin >> tc;
+	while(tc--){
+		int n, m;
+		cin >> n >> m;
+		vector<vector<int>>grid(n, vector<int>(m, -1));
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < m; j++){
+				cin >> grid[i][j];
+			}
+		}
+		Solution obj;
+		int ans = obj.orangesRotting(grid);
+		cout << ans << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
