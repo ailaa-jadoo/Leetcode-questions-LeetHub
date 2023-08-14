df2ac337d7b9046db1f2aa41682e3b2c4f0013fb
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-08-14 18:21:48 +0530

    Time: 84 ms (85.31%), Space: 67.6 MB (81.07%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -1,3 +1,5 @@
+// O(N) Kadane
+
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
@@ -15,3 +17,20 @@ public:
         return res;
     }
 };
+
+// O(N^2)
+
+// int maxSubArray(vector<int>& nums) {
+//         int n = nums.size();
+//         int res = INT_MIN;
+        
+//         for(int i=0;i<n;i++){
+//             int sum = 0;
+//             for(int j=i;j<n;j++){
+//                 sum += nums[j];
+//                 res = max(res, sum);
+//             }
+//         }
+        
+//         return res;
+//     }
