519243f01a7030a73c7123bed48c3966d2d521b3
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-10-02 17:45:15 +0530

    Time: 97 ms (98.34%), Space: 82.6 MB (21.12%) - LeetHub

--- 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
+++ 2419-longest-subarray-with-maximum-bitwise-and/2419-longest-subarray-with-maximum-bitwise-and.cpp
@@ -4,10 +4,10 @@ public:
         int maxi = INT_MIN, cnt = 0, res=1;
         
         for(int x:nums)
-            maxi = max(maxi, x);
+            maxi = max(maxi, x); //taking maxi value to have highest set bits
         
         for(int i=0;i<nums.size();i++){
-            if(nums[i]==maxi)
+            if(nums[i]==maxi)  //same number ke saath hi bit-wise AND largest hota hai
                 cnt++;
             else
                 cnt = 0;
