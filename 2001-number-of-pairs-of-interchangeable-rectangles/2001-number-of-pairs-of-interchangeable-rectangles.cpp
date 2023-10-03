a87b7bd83a472424f9b3955065d02e5b508c29ae
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-10-03 17:10:07 +0530

    Time: 404 ms (51.34%), Space: 138.1 MB (82.61%) - LeetHub

--- 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
+++ 2001-number-of-pairs-of-interchangeable-rectangles/2001-number-of-pairs-of-interchangeable-rectangles.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     long long interchangeableRectangles(vector<vector<int>>& rectangles) {
         long long count = 0;
-        unordered_map<double, long long> ratioCount;
+        map<double, long long> ratioCount;
 
         for (const auto& rect : rectangles) {
             double ratio = (double)rect[0] / rect[1];
