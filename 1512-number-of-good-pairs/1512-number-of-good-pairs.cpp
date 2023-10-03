2135ebbbed9c0c628d5f9ef2cc202502a077c055
Jado0o0o <76520626+ailaa-jadoo@users.noreply.github.com>
2023-10-03 16:53:29 +0530

    Time: 0 ms (100.00%), Space: 7.7 MB (7.10%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.cpp
@@ -4,7 +4,7 @@ public:
         int cnt=0;
         for(int i=0;i<nums.size();i++)
             for(int j=i+1;j<nums.size();j++)
-                if(nums[i] == nums[j] and i<j)
+                if(nums[i] == nums[j])
                     cnt++;
         
         return cnt++;
